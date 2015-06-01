d32df44f7b23d06e6640f8e5525ecb00610cbf71
Uwe Steinmann <steinm@debian.org>
2015-06-01 14:03:16 +0200

    fix wrong access on settings array

--- out/out.SetReviewersApprovers.php
+++ out/out.SetReviewersApprovers.php
@@ -48,7 +48,7 @@ if (!is_object($content)) {
 	UI::exitError(getMLText("document_title", array("documentname" => htmlspecialchars($document->getName()))),getMLText("invalid_version"));
 }
 
-if(!$this->settings->_enableVersionModification) {
+if(!$settings->_enableVersionModification) {
 	UI::exitError(getMLText("document_title", array("documentname" => htmlspecialchars($document->getName()))),getMLText("no_version_modification"));
 }
 
