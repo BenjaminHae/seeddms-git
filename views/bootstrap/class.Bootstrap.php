f02734b063f9d809a303f306a43336f6992abb17
Uwe Steinmann <steinm@debian.org>
2015-06-11 11:25:14 +0200

    show docs in root folder of document chooser

--- views/bootstrap/class.Bootstrap.php
+++ views/bootstrap/class.Bootstrap.php
@@ -1177,6 +1177,14 @@ function clearFilename<?php print $formName ?>() {
 				$node['children'] = array();
 			} else {
 				$node['children'] = jqtree($path, $folder, $this->params['user'], $accessmode, $showdocs, $expandtree, $orderby);
+				if($showdocs) {
+					$documents = $folder->getDocuments($orderby);
+					$documents = SeedDMS_Core_DMS::filterAccess($documents, $this->params['user'], $accessmode);
+					foreach($documents as $document) {
+						$node2 = array('label'=>$document->getName(), 'id'=>$document->getID(), 'load_on_demand'=>false, 'is_folder'=>false);
+						$node['children'][] = $node2;
+					}
+				}
 			}
 			$tree[] = $node;
 			
