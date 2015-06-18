2cdc9e3a1f60e81f2f180ed0d743a389535da767
Uwe Steinmann <steinm@debian.org>
2015-06-18 17:44:22 +0200

    fix some code which was intended for 5.0.x

--- SeedDMS_Core/Core/inc.ClassDMS.php
+++ SeedDMS_Core/Core/inc.ClassDMS.php
@@ -2126,9 +2126,9 @@ class SeedDMS_Core_DMS {
 
 		$versions = array();
 		foreach($resArr as $row) {
-			$document = new $this->classnames['document']($row['document'], '', '', '', '', '', '', '', '', '', '', '');
+			$document = new SeedDMS_Core_Document($row['document'], '', '', '', '', '', '', '', '', '', '', '');
 			$document->setDMS($this);
-			$version = new $this->classnames['documentcontent']($row['id'], $document, $row['version'], $row['comment'], $row['date'], $row['createdBy'], $row['dir'], $row['orgFileName'], $row['fileType'], $row['mimeType'], $row['fileSize'], $row['checksum']);
+			$version = new SeedDMS_Core_DocumentContent($row['id'], $document, $row['version'], $row['comment'], $row['date'], $row['createdBy'], $row['dir'], $row['orgFileName'], $row['fileType'], $row['mimeType'], $row['fileSize'], $row['checksum']);
 			if(!isset($versions[$row['dupid']])) {
 				$versions[$row['id']]['content'] = $version;
 				$versions[$row['id']]['duplicates'] = array();
