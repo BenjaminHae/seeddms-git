0aaf36bd0cf815208f001e1eedd5a5ee8207f8e9
Uwe Steinmann <steinm@debian.org>
2015-07-06 21:54:03 +0200

    make field for expired date larger

--- views/bootstrap/class.SetExpires.php
+++ views/bootstrap/class.SetExpires.php
@@ -58,7 +58,7 @@ class SeedDMS_View_SetExpires extends SeedDMS_Bootstrap_Style {
 	<td><?php printMLText("expires");?>:</td>
 	<td>
     <span class="input-append date span12" id="expirationdate" data-date="<?php echo $expdate; ?>" data-date-format="dd-mm-yyyy" data-date-language="<?php echo str_replace('_', '-', $this->params['session']->getLanguage()); ?>">
-      <input class="span4" size="16" name="expdate" type="text" value="<?php echo $expdate; ?>">
+      <input class="span6" name="expdate" type="text" value="<?php echo $expdate; ?>">
       <span class="add-on"><i class="icon-calendar"></i></span>
     </span><br />
     <label class="checkbox inline">
