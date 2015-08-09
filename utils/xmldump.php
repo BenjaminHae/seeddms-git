1cf79c6cbea5e97b56aaa1c324781847e9aec235
Uwe Steinmann <steinm@debian.org>
2015-08-09 07:25:26 +0200

    set $type2 to $type in dumplog()

--- utils/xmldump.php
+++ utils/xmldump.php
@@ -116,7 +116,7 @@ function dumplog($version, $type, $logs, $indent) { /* {{{ */
 		$type2 = 'approve';
 		break;
 	default:
-		$type2 = 'review';
+		$type2 = $type;
 	}
 	echo $indent."   <".$type."s>\n";
 	$curid = 0;
